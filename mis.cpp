e4f144369bc896c848d1b5c47c49decd16c4d701
imjohnzakkam <johnzakkam2592@gmail.com>
2020-04-10 15:26:52 +0530

    indents

--- mis.cpp
+++ mis.cpp
@@ -7,11 +7,6 @@
 #define nl cout << endl;
 using namespace std; 
 
-
-
-/* A binary tree node has data, pointer 
-to left child and a pointer to 
-right child */
 class node 
 { 
 	public: 
@@ -20,9 +15,6 @@ class node
 	node *left, *right; 
 }; 
 
-// A memoization function returns size 
-// of the largest independent set in 
-// a given binary tree 
 int LISS(node *root) 
 { 
 	if (root == NULL) 
@@ -33,24 +25,16 @@ int LISS(node *root)
 
 	if (root->left == NULL && root->right == NULL) 
 		return (root->liss = 1); 
-
-	// Calculate size excluding the current node 
 	int liss_excl = LISS(root->left) + LISS(root->right); 
-
-	// Calculate size including the current node 
 	int liss_incl = 1; 
 	if (root->left) 
 		liss_incl += LISS(root->left->left) + LISS(root->left->right); 
 	if (root->right) 
 		liss_incl += LISS(root->right->left) + LISS(root->right->right); 
-
-	// Maximum of two sizes is LISS, store it for future uses. 
 	root->liss = max(liss_incl, liss_excl); 
-
 	return root->liss; 
 } 
 
-// A utility function to create a node 
 node* newNode(int data) 
 { 
 	node* temp = new node(); 
@@ -60,23 +44,16 @@ node* newNode(int data)
 	return temp; 
 } 
 
-// Driver code 
-int main() 
+int32_t main() 
 { 
-	// Let us construct the tree 
-	// given in the above diagram 
-	node *root	 = newNode(20); 
-	root->left		 = newNode(8); 
-	root->left->left	 = newNode(4); 
-	root->left->right	 = newNode(12); 
-	root->left->right->left = newNode(10); 
+	node *root	             = newNode(20); 
+	root->left		         = newNode(8); 
+	root->left->left	     = newNode(4); 
+	root->left->right	     = newNode(12); 
+	root->left->right->left  = newNode(10); 
 	root->left->right->right = newNode(14); 
-	root->right		 = newNode(22); 
-	root->right->right	 = newNode(25); 
-
+	root->right		         = newNode(22); 
+	root->right->right	     = newNode(25); 
 	cout << "Size of the Largest Independent Set is " << LISS(root); 
-
 	return 0; 
 } 
-
-// This code is contributed by rathbhupendra 

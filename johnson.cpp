0f89e4edda04afdaa8f6f29d5378e96ab9ea0f61
Yoga Sri Varshan V <yogasrivarshan@gmail.com>
2020-04-13 16:45:07 +0530

    Update johnson.cpp

--- johnson.cpp
+++ johnson.cpp
@@ -1,162 +1,117 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<list>
-#include<queue>
-#include<cstdlib>
-#define INF 0x3f3f3f3f
+#include<bits/stdc++.h>
+#define ll long long int
+#define inf INT_MAX
+#define pb push_back
 using namespace std;
-int *djk_dist;
-class cmp{
-public:
-    bool operator()(int a,int b){
-        return (djk_dist[a]>djk_dist[b]);
-    }
-};
-void add_edge(vector<list<pair<int,int> > > &adj_list,int src,int dest, int wt){
-    adj_list[src].push_back(make_pair(dest,wt));
+ll n = 4;
+void dijkstra(ll g[][4],ll v){
+	map<ll,ll>m;
+	ll i;
+	for(i=0;i<n;i++)
+		m[i]=0;
+	m[v]=1;
+	ll a[n];
+	for(i=0;i<n;i++){
+		a[i]=g[v][i];
+	}
+	for(int j=1;j<n;j++){
+		ll min = inf,w=inf;
+		for(i=0;i<n;i++){
+			if(a[i]<=min && m[i]==0){
+				w = i;
+				min = a[i];
+			}
+		}
+		m[w]=1;
+		for(i=0;i<n;i++){
+			if(a[w]+g[w][i]<a[i] && m[i]==0)
+				a[i]=a[w]+g[w][i];
+		}
+	}
+	for(i=0;i<n;i++){
+		if(a[i]!=INT_MAX)
+				cout<<a[i]<<" ";
+		else
+			cout<<"inf"<<" ";
+	}
+	cout<<endl;
 }
-void change_weights(vector<list<pair<int,int> > > &adj_list,int *h){
-    int v = adj_list.size(),i;
-    list<pair<int,int> > :: iterator itr;
-    for(i=0;i<v;i++){
-        for(itr=adj_list[i].begin();itr!=adj_list[i].end();itr++){
-            itr->second += h[i] - h[itr->first];
-        }
-    }
+vector<pair<pair<ll,ll>,ll>>edges;
+vector<ll>weights;
+void bellmanford(ll g[][4]){
+	ll i,j;
+	for(i=0;i<n+1;i++)
+		weights.push_back(0);
+	for(i=0;i<n;i++){
+		pair<ll,ll> p;
+		p.first = n;
+		p.second = i;
+		pair<pair<ll,ll>,ll>p1;
+		p1.first = p;
+		p1.second = 0;
+		edges.pb(p1);
+	}
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(g[i][j]!=INT_MAX && i!=j){
+				pair<ll,ll> p;
+				p.first = i;
+				p.second = j;
+				pair<pair<ll,ll>,ll>p1;
+				p1.first = p;
+				p1.second = g[i][j];
+				edges.pb(p1);			
+			}
+		}
+	}
+	for(j=0;j<edges.size();j++){
+		cout<<edges[j].first.first<<","<<edges[j].first.second<<" "<<edges[j].second<<endl; 
+	}
+	for(i=0;i<n+1;i++){
+		for(j=0;j<edges.size();j++){
+			if(weights[edges[j].first.second]>weights[edges[j].first.first] + edges[j].second)
+				weights[edges[j].first.second] = weights[edges[j].first.first] + edges[j].second;
+		}
+	}
+	for(i=0;i<n;i++)
+		cout<<weights[i]<<" ";
+	cout<<endl;
+	cout<<"The original graph is"<<endl;
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(g[i][j]!=INT_MAX)
+				cout<<g[i][j]<<" ";
+			else
+				cout<<"inf"<<" ";
+		}
+		cout<<endl;
+	}
+	for(j=n;j<edges.size();j++){
+		cout<<edges[j].first.first<<","<<edges[j].first.second<<" "<<edges[j].second<<endl; 	
+		edges[j].second = edges[j].second + weights[edges[j].first.first] - weights[edges[j].first.second]; 
+		g[edges[j].first.first][edges[j].first.second] = edges[j].second; 	
+	}
+	cout<<"The modified graph is"<<endl;
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(g[i][j]!=INT_MAX)
+				cout<<g[i][j]<<" ";
+			else
+				cout<<"inf"<<" ";
+		}
+		cout<<endl;
+	}
 }
-int * bellman_ford_johnson(vector<list<pair<int,int> > > &adj_list){
-    int v = adj_list.size();
-    list<pair<int,int> > :: iterator itr;
-    int i,j;
-    list<pair<int,int> > src_list;
-    for(i=0;i<v;i++){
-        src_list.push_back(make_pair(i,0));
-    }
-    adj_list.push_back(src_list);
-    int src = v;
-    v++;/*sizeof(adj_list) now is v+1*/
-    int *dist;
-    dist = (int *)calloc(v,sizeof(int));
-    for(i=0;i<v;i++)dist[i]=INF;
-    dist[src] = 0;
-    bool f=0;
-    for(i=0;i<v-1;i++){
-        /*Now traverse all edges in adj_list*/
-        f=0;
-        for(j=0;j<v;j++){
-            if(dist[j]!=INF){
-                for(itr= adj_list[j].begin();itr!=adj_list[j].end();itr++){
-                    if(dist[itr->first]>dist[j]+itr->second){
-                        dist[itr->first] = dist[j]+itr->second;
-                        f=1;
-                    }
-                }
-            }
-        }
-        if(!f)break;
-    }
-    f=0;
-    for(j=0;j<v;j++){
-            for(itr= adj_list[j].begin();itr!=adj_list[j].end();itr++){
-                if(dist[itr->first]>dist[j]+itr->second){
-                    dist[itr->first] = dist[j]+itr->second;
-                    f=1;
-                }
-            }
-    }
-    if(f){
-        return NULL;
-    }
-    adj_list.pop_back();
-    return dist;
-}
-int *dijkstra(vector<list<pair<int,int> > > &adj_list, int src){
-    int v = adj_list.size(), i;
-    djk_dist = (int *)calloc(v, sizeof(int));
-    bool sptSet[v];
-    for(i=0;i<v;i++){
-        djk_dist[i]=INF;
-        sptSet[i]=false;
-    }
-    djk_dist[src]=0;
-    priority_queue<int,vector<int>,cmp> pq;
-    for(i=0;i<v;i++)pq.push(i);
-    int node;
-    list<pair<int,int> > :: iterator itr;
-    while(!pq.empty()){
-        node = pq.top();
-        sptSet[node]=true;
-        for(itr=adj_list[node].begin();itr!=adj_list[node].end();itr++){
-            if(!sptSet[itr->first] && djk_dist[itr->first]>djk_dist[node]+itr->second){
-                djk_dist[itr->first]=djk_dist[node]+itr->second;
-            }
-        }
-        pq.pop();
-    }
-    return djk_dist;
-}
-void print_adj_list(vector<list<pair<int,int> > > adj_list){
-    int v = adj_list.size(),i;
-    list<pair<int,int> >::iterator itr;
-    for(i=0;i<v;i++){
-        for(itr =adj_list[i].begin();itr!=adj_list[i].end();itr++)cout << itr->second << ' ';
-        cout << endl;
-    }
-}
-void johnson_spa(vector<list<pair<int,int> > > adj_list){
-
-    /*Get weights to assign to each vertex for readjusting each edge wt to make them positive and hence be able to apply djk*/
-    int * h = bellman_ford_johnson(adj_list), i, j;
-    int v = adj_list.size();
-    if(!h){cout << "There is a negative cycle in graph"<<endl;return;}
-    /*cout << "Before changing weights"<<endl;
-    print_adj_list(adj_list);
-    */
-    change_weights(adj_list,h);
-    /*cout << "After changing weights"<<endl;
-    print_adj_list(adj_list);
-    cout << "h vector is as follow : ";
-    for(i=0;i<v;i++)cout << h[i]<< ' ';
-    cout<<endl;
-    */
-    /*Now apply djk*/
-
-    int **distances = (int **)calloc(v,sizeof(int *));
-    for(i=0;i<v;i++){
-         distances[i] = dijkstra(adj_list,i);
-         /*we need to revert back the addition*/
-         for(j=0;j<v;j++)if(i!=j && distances[i][j]!=INF)distances[i][j]+=-h[i]+h[j];
-    }
-    cout << "Distance Matrix is"<<endl;
-    for(i=0;i<v;i++){
-        for(j=0;j<v;j++){
-            if(distances[i][j]!=INF)
-            cout << distances[i][j] << ' ';
-            else
-            cout << "INF ";
-        }
-        cout << endl;
-    }
+void apspgreedy(ll g[][4]){
+	ll i,j,k;
+	bellmanford(g);
+	cout<<"The shortest paths are"<<endl;
+	for(i=0;i<n;i++){
+		dijkstra(g,i);
+	}
 }
 int main(){
-    /*
-    Copy Input
-    4 5
-    0 1 -5
-    1 2 4
-    2 3 1
-    0 3 3
-    0 2 2
-    
-    */
-    int i, n, e, src, dest, wt, j;
-    cin >> n >> e;
-    vector<list<pair<int,int> > > adj_list(n);
-    for(i=0;i<e;i++){
-        cin >> src >> dest >> wt;
-        add_edge(adj_list,src,dest,wt);
-    }
-    johnson_spa(adj_list);
-    return 0;
+	ll g[4][4]={{0,3,inf,7},{8,0,2,inf},{5,inf,0,1},{2,inf,inf,0}};
+	apspgreedy(g);
+	return 0;
 }
